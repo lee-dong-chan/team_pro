2ff3c653040a0aa2de1a4f8452ef5bea835b1918
lee-dong-chan <dsongc3082@naver.com>
2024-05-26 11:34:20 +0900

    feat:mainpage

--- front/product_list/index.js
+++ front/product_list/index.js
@@ -1,3 +1,84 @@
+const catelist1Elem = document.getElementById("cate-list1");
+const catelist2Elem = document.getElementById("cate-list2");
+const catelist3Elem = document.getElementById("cate-list3");
+(async () => {
+  try {
+    const mainpage = (await axios.get("http://localhost:8080/product_list", {}))
+      .data;
+
+    //category
+    if (mainpage[0]) {
+      mainpage[0].forEach((cate1) => {
+        catelist1Elem.innerHTML += `<a href="/product_list?cate1=${cate1.id}" class="cate-list">${cate1.name}</a>`;
+      });
+      for (let i = 0; i < catelist1.length; i++) {
+        catelist1[i].onmouseover = () => {
+          secondCateElem.classList.add("on");
+          let secondcate = i;
+          let str = "";
+          if (mainpage[0][secondcate].Secondcategories) {
+            mainpage[0][secondcate].Secondcategories.forEach((cate2) => {
+              str += `<a href="/product_list?cate2=${cate2.id}" class="cate-list">${cate2.name}</a>`;
+            });
+          }
+          catelist2Elem.innerHTML = str;
+          for (let i = 0; i < catelist2.length; i++) {
+            catelist2[i].onmouseover = () => {
+              thirdCateElem.classList.add("on");
+              let thirdcate = i;
+              let str = "";
+              if (
+                mainpage[0][secondcate].Secondcategories[thirdcate]
+                  .Thirdcategories
+              ) {
+                mainpage[0][secondcate].Secondcategories[
+                  thirdcate
+                ].Thirdcategories.forEach((cate3) => {
+                  str += `<a href="/product_list?cate3=${cate3.id}" class="cate-list">${cate3.name}</a>`;
+                });
+              }
+              catelist3Elem.innerHTML = str;
+            };
+
+            catelist2[i].onmousedown = () => {
+              for (let a = 0; a < catelist1.length; a++) {
+                catelist2[a].classList.remove("on");
+              }
+              catelist2[i].classList.add("on");
+            };
+          }
+        };
+
+        catelist1[i].onmousedown = () => {
+          for (let a = 0; a < catelist1.length; a++) {
+            catelist1[a].classList.remove("on");
+          }
+          catelist1[i].classList.add("on");
+        };
+      }
+    }
+    //
+    let urlStr = window.location.href;
+    let url = new URL(urlStr);
+    let urlparams = url.searchParams;
+    let cate1 = urlparams.get("cate1");
+    let cate2 = urlparams.get("cate2");
+    let cate3 = urlparams.get("cate3");
+
+    if (cate1) {
+      console.log("cate1");
+    }
+    if (cate2) {
+      console.log("cate2");
+    }
+    if (cate3) {
+      console.log("cate3");
+    }
+  } catch (err) {
+    console.error(err);
+  }
+})();
+
 //paging script
 const pagelistElem = document.getElementsByClassName("page-list")[0];
 const pageElem = document.getElementsByClassName("num");
