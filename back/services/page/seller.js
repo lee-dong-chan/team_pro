d9b40e4aaf07ae49ea80c84e0192c62d9593cc76
jiwanbang <jiwanbang@naver.com>
2024-05-29 08:49:52 +0900

    seller_input_working

--- back/services/page/seller.js
+++ back/services/page/seller.js
@@ -1,10 +1,31 @@
-import { sequelize, Product } from "../../models/index.js";
+import {
+  sequelize,
+  Product,
+  Productinfo,
+  Productsell,
+  Product_tag,
+  Prd_img,
+} from "../../models/index.js";
 
 export default async (req, res) => {
   try {
-    console.log(req.body);
     res.json();
-    Product.create(req.body);
+
+    await Product.create(req.body);
+    const prd = await Product.findOne({
+      include: [
+        {
+          model: Product,
+        },
+      ],
+    });
+    console.dir("prd : ", prd.id);
+    const prdSell = await Productinfo.create(req.body);
+    await prdSell.addComment(prd.id);
+
+    // await Productsell.create(req.body);
+    // await Product_tag.create(req.body);
+    // Prd_img.create(req.body);
   } catch (err) {
     console.log(err);
     res.send("error");
