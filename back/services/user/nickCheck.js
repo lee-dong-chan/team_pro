e56d93629c958e951d9e8be329433667a1a00605
lee-dong-chan <dsongc3082@naver.com>
2024-05-29 13:41:25 +0900

    feat:login

--- back/services/user/nickCheck.js
+++ back/services/user/nickCheck.js
@@ -1,17 +1,20 @@
 import User from "../../models/user/user.js";
 export default async (req, res) => {
   try {
-    // if (req.body.nick) {
-    //   const reqnick = req.body.nick;
-    //   const dubnick = await User.findOne({
-    //     where: { nickname: reqnick },
-    //   });
-    //   console.log(dubnick.nickname);
-    //   if (dubnick.nickname == req.body.nick) {
-    //     return res.json({ message: "ok" });
-    //   }
-    // }
+    if (req.body.nick) {
+      const reqnick = req.body.nick;
+      const dubnick = await User.findOne({
+        where: { nickname: reqnick },
+      });
+      console.log(dubnick.nickname);
+      if (dubnick.nickname == req.body.nick) {
+        res.json({ message: "중복임" });
+      } else {
+        res.send("ok");
+      }
+    }
   } catch (err) {
     console.log(err);
+    res.send("ok");
   }
 };
