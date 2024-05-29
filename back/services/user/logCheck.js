861b9c48a428b9bd7dbb787a958951e72e5061c0
lee-dong-chan <dsongc3082@naver.com>
2024-05-29 22:58:14 +0900

    feat:service

--- back/services/user/logCheck.js
+++ back/services/user/logCheck.js
@@ -1,10 +1,19 @@
 import { User } from "../../models/index.js";
-
+import { Userstore } from "../../models/index.js";
 export default async (req, res, next) => {
   try {
+    console.log(req.session.user);
     if (req.session.user) {
-      req.user = await User.findOne({
+      req.user = await User.findAll({
+        attributes: ["id"],
+
         where: { id: req.session.user },
+        include: [
+          {
+            model: Userstore,
+            attributes: ["id"],
+          },
+        ],
       });
     }
   } catch (err) {
