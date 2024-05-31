b9d307b07a377b9945a5ff1b1bd88ae62f9a50e4
lee-dong-chan <163073181+lee-dong-chan@users.noreply.github.com>
2024-06-01 02:52:32 +0900

    Revert "seller_page_work"
    This reverts commit 00128125f12a86fff68d4b01efeaf00b0c879dd7.

--- front/seller/seller_page.js
+++ front/seller/seller_page.js
@@ -1,3 +1,30 @@
+const imgArea = document.getElementById("img_area");
+const inputBtn = document.getElementById("prd_img");
+inputBtn.onchange = (e) => {
+  for (var image of e.target.files) {
+    let reader = new FileReader();
+
+    reader.onload = (e) => {
+      let img = document.createElement("img");
+      img.setAttribute("src", e.target.result);
+      imgArea.appendChild(img);
+    };
+
+    console.log(image);
+    reader.readAsDataURL(image);
+  }
+
+  //   let preview = new FileReader();
+
+  //   preview.onload = (e) => {
+  //     let img = document.createElement("img");
+  //     img.setAttribute("src", e.target.result);
+  //     imgArea.appendChild(img);
+  //   };
+
+  //   preview.readAsDataURL(e.target.files[0]);
+};
+
 // let cateAdd = document.getElementById("category_ul");
 // cateArr = [
 //   "여성의류",
@@ -37,93 +64,3 @@
 //   console.log(cateArr[i]);
 //   cateAdd.innerHTML = `<li>${cateArr[i]}</li>`;
 // }
-
-const imgArea = document.getElementById("img_area");
-const inputBtn = document.getElementById("prd_img");
-inputBtn.onchange = (e) => {
-  for (var image of e.target.files) {
-    let reader = new FileReader();
-
-    reader.onload = (e) => {
-      let img = document.createElement("img");
-      img.setAttribute("src", e.target.result);
-      imgArea.appendChild(img);
-    };
-
-    console.log(e.target.files);
-    reader.readAsDataURL(image);
-  }
-
-  //   let preview = new FileReader();
-
-  //   preview.onload = (e) => {
-  //     let img = document.createElement("img");
-  //     img.setAttribute("src", e.target.result);
-  //     imgArea.appendChild(img);
-  //   };
-
-  //   preview.readAsDataURL(e.target.files[0]);
-};
-
-const namelength = document.getElementById("name_length");
-const nameInput = document.getElementById("nameinput");
-const form = document.forms.create_prd;
-const detail = document.getElementById("prd_detail");
-const detailLength = document.getElementById("detail_length");
-
-const statusRadio = document.getElementsByName("prd_quality");
-
-let statusValue = document.querySelector(
-  'input[name="prd_quality"]:checked'
-).value;
-function radioFnc(e) {
-  console.log(e.target.value);
-}
-
-form.sizeSelecter.oninput = (e) => {
-  console.log(e.target.value);
-};
-
-nameInput.oninput = (e) => {
-  if (e.target.value.length > 0) {
-    isnameEmpty = true;
-    namelength.innerHTML = `${e.target.value.length}/40`;
-    console.log(isnameEmpty);
-  } else if (e.target.value.length < 1) {
-    isnameEmpty = false;
-    console.log(isnameEmpty);
-  }
-};
-
-detail.oninput = (e) => {
-  detailLength.innerHTML = `${e.target.value.length}/2000`;
-};
-
-let status = 1;
-
-form.onsubmit = async (e) => {
-  e.preventDefault();
-  try {
-    const sellData = (
-      await axios.post(
-        "/localhost/seller",
-        {
-          img: form.prd_img.files,
-          prd_name: form.nameinput.value,
-          size: form.sizeSelecter.value,
-          prd_quality: statusValue,
-          product_explanation: form.prd_detail.value,
-          price: form.price_input.value,
-        },
-        {
-          // options
-          withCredentials: true,
-        }
-      )
-    ).data;
-    console.log(sellData);
-    location.href = "localhost/main";
-  } catch (err) {
-    console.error(err);
-  }
-};
