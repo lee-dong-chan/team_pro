9b5a81b0bde343ec8ed70af82345db9b32200fb1
lee-dong-chan <163073181+lee-dong-chan@users.noreply.github.com>
2024-06-01 02:52:32 +0900

    Revert "category_btn"
    This reverts commit b19ae58be0c1e59fdf9f5cf0cbebd9fe7d17d3dc.

--- back/services/page/seller.js
+++ back/services/page/seller.js
@@ -1,10 +1,7 @@
-import { sequelize, Product } from "../../models/index.js";
-
 export default async (req, res) => {
   try {
     console.log(req.body);
     res.json();
-    Product.create(req.body);
   } catch (err) {
     console.log(err);
     res.send("error");
