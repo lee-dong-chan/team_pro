94ea061bab52b617b727e7829c10c6b1da26f3b0
lee-dong-chan <163073181+lee-dong-chan@users.noreply.github.com>
2024-06-01 02:52:32 +0900

    Revert "feat:category"
    This reverts commit 887d5d02e1ca86312fb16b421bdfde3c8492b636.

--- back/services/page/main.js
+++ back/services/page/main.js
@@ -2,42 +2,26 @@ import {
   Firstcategory,
   Secondcategory,
   Thirdcategory,
-  Product,
-  Productsell,
-  Prd_img,
 } from "../../models/index.js";
 
 export default async (req, res) => {
   try {
     const category = await Firstcategory.findAll({
-      attributes: ["id", "name"],
+      attributes: ["name"],
       include: [
         {
           model: Secondcategory,
-          attributes: ["id", "name", "firstcategory_id"],
+          attributes: ["name", "firstcategory_id"],
           include: [
             {
               model: Thirdcategory,
-              attributes: ["id", "name", "secondcategory_id"],
+              attributes: ["name", "secondcategory_id"],
             },
           ],
         },
       ],
     });
-    const product = await Product.findAll({
-      attributes: ["name", "created_at"],
-      include: [
-        {
-          model: Productsell,
-          attributes: ["price"],
-        },
-        {
-          model: Prd_img,
-          attributes: ["img_path"],
-        },
-      ],
-    });
-    res.json([category, product]);
+    res.json(category);
   } catch (err) {
     console.log(err);
     res.send("error");
